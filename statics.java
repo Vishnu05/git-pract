68775a8f39417414704faab81aea02b92e4d390e
= <vishnuthamiz96@gmail.com>
2020-02-02 08:24:49 +0530

    tags

---
+++ statics.java
@@ -0,0 +1,13 @@
+import javax.sound.sampled.SourceDataLine;
+
+class statics {
+
+    static {
+        System.out.println("static block");     
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+
+}
