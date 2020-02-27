e929fc8b7f98cf6f2054394f0afeccf6a4b32579
Vishnu <vthamiz5@gmail.com>
2020-02-27 11:28:42 +0530

    commit message

--- Serialization.java
+++ Serialization.java
@@ -13,21 +13,20 @@ public class Serialization {
         // Arrays.sort();
 
         // "json":"object"
-    
 
-    // serialization
-    // stash in comment
-    
-    System.out.println("merge conflicts for the git and meregetool");
+        // serialization
+        // stash in comment
+
+        System.out.println("merge conflicts for the git and meregetool");
+        System.out.println("new commit to merge");
     }
 }
 
-// stash is better when we write 
+// stash is better when we write
 
 // %hspU6C6Y7Ig
 
 // console.log('git mergetool check')
-
 
 // stash is better when we write
 
