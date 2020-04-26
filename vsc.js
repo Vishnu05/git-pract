13d46f21cd533bf9ff2deff161912a92f1f2c176
Vishnu T <vishnuthamiz96@gmail.com>
2020-04-26 10:34:48 +0530

    genral html file created

--- vsc.js
+++ vsc.js
@@ -16,16 +16,17 @@ function name(name) {
 
 console.log(name(name))
 
-let a = 'chrome dev tool'
-let a = 'syntax error'
+// let a = 'chrome dev tool'
+// let a = 'syntax error'
 
 function name(value) {
-
+    /** call back function as per my understandin */
     return hello(value)
 }
 
 function hello(value) {
-    return 'hello' + value
+
+    return 'hello ' + value
 }
 
 console.log(name('world'))
